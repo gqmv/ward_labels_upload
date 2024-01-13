4ed91e1021f4a8288952b6581b410d8802639b53
Gabriel Queiroz <gqmv@cin.ufpe.br>
2024-01-13 14:49:51 -0300

    AAA

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     author_email="<gabriel.vieira@vdspar.com>",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     packages=find_packages(),
     install_requires=["pydantic", "requests", "tqdm"],
     keywords=["Internal"],
