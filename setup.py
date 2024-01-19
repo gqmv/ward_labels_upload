4775a801d01cc5e59b3846c400ef7854a9139026
Gabriel Queiroz <gqmv@cin.ufpe.br>
2024-01-19 17:14:47 -0300

    aa

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 DESCRIPTION = "An internal package to allow for easy uploading of labels to the Ward Analytics API."
 LONG_DESCRIPTION = (
     "An internal package to allow for easy uploading of labels to the Ward Analytics API."
